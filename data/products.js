07a1d717c3688abc11d66cd6ab28928c72c37767
Mert <mertuygur02@gmail.com>
2021-07-30 22:49:48 +0300

    server side initialized.

---
+++ data/products.js
@@ -0,0 +1,33 @@
+const products = [
+    {
+        name: 'Sticker 1 Example',
+        image: '/images/stickers/st1.svg',
+        description: 'Explanation have to be made',
+        category: 'Cyber',
+        dimensions: '8cm x 8cm',
+    },
+    {
+        name: 'Example of Sticker 2',
+        image: '/images/stickers/st2.svg',
+        description: 'I am here buut why',
+        category: 'Software',
+        dimensions: '8cm x 8cm'
+
+    },
+    {
+        name: 'This is the third',
+        image: '/images/stickers/st3.svg',
+        description: 'Laughing',
+        category: 'Nature',
+        dimensions: '8cm x 8cm'
+    },
+    {
+        name: 'The last one',
+        image: '/images/stickers/st4.svg',
+        description: 'Lorem ipsum dolor sit amet',
+        category: 'Design',
+        dimensions: '8cm x 8cm'
+    }
+]
+
+export default products;
